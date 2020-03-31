dbe9d729a6281db9cbfd8321cd5ececd46c4e654
steven <slamslamslam12411@gmail.com>
2020-03-31 22:08:33 +0800

    [新增] 登入頁面Layout，kt檔，資源檔

--- app/src/main/java/com/example/numberplate_10/activity/LoginActivity.kt
+++ app/src/main/java/com/example/numberplate_10/activity/LoginActivity.kt
@@ -2,10 +2,12 @@ package com.example.numberplate_10.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import com.example.numberplate_10.R
+import com.example.numberplate_10.conn.ApiServices
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener{
 
@@ -16,7 +18,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        Log.d("liao", ApiServices.LOGIN.service)
         initElements()
     }
 
---
+++ app/src/main/java/com/example/numberplate_10/config/AppConstants.kt
@@ -0,0 +1,10 @@
+package com.example.numberplate_10.config
+
+object AppConstants {
+    const val LOG_TAG = "LIAO"
+
+    object API {
+        const val BASE_URL = ""
+        const val SOCKET_PORT = "";
+    }
+}
