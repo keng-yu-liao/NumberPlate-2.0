da4251a6abb517669681b5d1bda8b16617549b5a
steven <slamslamslam12411@gmail.com>
2020-05-04 06:35:37 +0800

    [新增] RemoteCallActivity
    。遙控叫號頁面
    。文字大小dimens檔

---
+++ app/src/main/java/com/example/numberplate_10/ui/section/remoteCall/RemoteCallActivity.kt
@@ -0,0 +1,13 @@
+package com.example.numberplate_10.ui.section.remoteCall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.numberplate_10.R
+
+class RemoteCallActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_remote_call)
+    }
+}
