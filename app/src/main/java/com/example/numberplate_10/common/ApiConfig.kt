e2a3db844c7a86cb1406fbfae48b1b4714c9858e
steven <slamslamslam12411@gmail.com>
2020-09-20 13:10:22 +0800

    [調整] 更換Server API URL

--- app/src/main/java/com/example/numberplate_10/common/ApiConfig.kt
+++ app/src/main/java/com/example/numberplate_10/common/ApiConfig.kt
@@ -6,7 +6,7 @@ object ApiConfig {
     object API {
 
         //APP 設定參數
-        const val BASE_API_URL: String = "http://35.229.169.52/NumberPlate-1.0-Server/"
+        const val BASE_API_URL: String = "http://35.229.169.52/NumberPlate-1.0-Server/app/"
         const val BASE_WEB_URL: String = "http://35.229.169.52/NumberPlate-1.0-Web/number_plate_10.html?storeTableName=*STORE_TABLE_NAME&yourNum=*YOUR_NUM"
         const val STORE_TABLE: String = "store_test_num_status"
         const val TIMEOUT_CONNECT = 5L
