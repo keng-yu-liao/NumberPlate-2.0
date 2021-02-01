03a9de80d5fa23c558b77da741b9dea974d55bfc
steven <slamslamslam12411@gmail.com>
2021-02-02 07:39:53 +0800

    [新增] 2代登入頁面

---
+++ app/src/main/java/com/example/numberplate_20/ui/section/login/Login2Activity.kt
@@ -0,0 +1,13 @@
+package com.example.numberplate_20.ui.section.login
+
+import android.os.Bundle
+import com.example.numberplate_20.R
+import com.example.numberplate_20.ui.base.BaseActivity
+
+class Login2Activity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_2)
+    }
+}
